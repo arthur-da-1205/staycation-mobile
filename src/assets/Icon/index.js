1f672fdf3003bb8ddbeea22cd9801ae4f386aa9c
unknown <izzy.zammil@gmail.com>
2022-03-31 23:41:47 +0700

    Fixing WelcomeScreenOne

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -1,4 +1,6 @@
 import OnBack from './onBack.svg';
 import EditProfile from './editProfile.svg';
+import OnNext from './OnNext.svg';
+import CarouselOne from './carouselOne.svg';
 
-export {OnBack, EditProfile};
+export {OnBack, EditProfile, OnNext, CarouselOne};
--- src/screens/WelcomeScreenOne/index.js
+++ src/screens/WelcomeScreenOne/index.js
@@ -1,61 +1,102 @@
-import {StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import {COLORS} from '../../constants';
 import {Header, Space} from '../../components';
-import {WelcomeImg1, WelcomeImg2, WelcomeImg3} from '../../assets';
+import {
+  CarouselOne,
+  OnNext,
+  WelcomeImg1,
+  WelcomeImg2,
+  WelcomeImg3,
+} from '../../assets';
 
 const WelcomeScreenOne = () => {
   return (
     <View style={styles.page}>
-      {/* <Header title={'Forget busy work, start next vacation'} /> */}
-      {/* <Space height={28} /> */}
-      <ScrollView
-        horizontal
-        //   showsHorizontalScrollIndicator={false}
-        style={{flex: 1, backgroundColor: 'blue'}}>
-        <Space width={18} />
-        <Image
-          source={WelcomeImg1}
-          style={{
-            height: 250,
-            width: 148,
-            marginHorizontal: 6,
-          }}
-        />
-        <Image
-          source={WelcomeImg2}
-          style={{
-            height: 250,
-            width: 148,
-            marginHorizontal: 6,
-          }}
-        />
-        <Image
-          source={WelcomeImg3}
-          style={{
-            height: 250,
-            width: 148,
-            marginHorizontal: 6,
-          }}
-        />
-        <Space width={18} />
-      </ScrollView>
+      <Header title={'Forget busy work, start next vacation'} />
+      <View>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{marginVertical: 28}}>
+          <Space width={18} />
+          <Image
+            source={WelcomeImg1}
+            style={{
+              height: 250,
+              width: 148,
+              marginHorizontal: 6,
+            }}
+          />
+          <Image
+            source={WelcomeImg2}
+            style={{
+              height: 250,
+              width: 148,
+              marginHorizontal: 6,
+            }}
+          />
+          <Image
+            source={WelcomeImg3}
+            style={{
+              height: 250,
+              width: 148,
+              marginHorizontal: 6,
+            }}
+          />
+          <Space width={18} />
+        </ScrollView>
+      </View>
       <View
         style={{
-          flex: 1,
           paddingHorizontal: 24,
-          backgroundColor: 'yellow',
-          //   marginTop: 150,
+          flex: 1,
         }}>
         <Text
           style={{
             textAlign: 'center',
             fontFamily: 'Poppins-Regular',
             color: COLORS.gray,
+            lineHeight: 24,
           }}>
           We provide what you need to enjoy your holiday with family. Time to
           make another memorable moments.
         </Text>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginHorizontal: 24,
+          marginBottom: 24,
+        }}>
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            width: 4,
+            height: 4,
+            opacity: 0,
+          }}
+        />
+        <View style={{marginLeft: 16}}>
+          <CarouselOne />
+        </View>
+        <TouchableOpacity
+          style={{
+            paddingLeft: 16,
+            paddingVertical: 6,
+          }}>
+          <OnNext />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -64,5 +105,5 @@ const WelcomeScreenOne = () => {
 export default WelcomeScreenOne;
 
 const styles = StyleSheet.create({
-  page: {backgroundColor: COLORS.red, flex: 1},
+  page: {backgroundColor: COLORS.white, flex: 1},
 });
