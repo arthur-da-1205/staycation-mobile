cc1460090ea902118644a12ea0a3cc57a775a177
arthur-da-1205 <arthur.darwanto@gmail.com>
2022-04-09 00:07:30 +0700

    fixing

--- src/screens/HouseScreen/index.js
+++ src/screens/HouseScreen/index.js
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import React from 'react';
+import {CategoriesCard, Header, Space} from '../../components';
 
 const HouseScreen = () => {
   return (
-    <View>
-      <Text>HouseScreen</Text>
+    <View style={styles.page}>
+      <Header title="Hotel" type="onBack" />
+      <Space height={24} />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <CategoriesCard dummyImg={DummyHotelScreen} title="Mariejoa" />
+        <CategoriesCard dummyImg={DummyHotelScreen} title="Marineford" />
+        <CategoriesCard dummyImg={DummyHotelScreen} title="Impel Down" />
+        <CategoriesCard dummyImg={DummyHotelScreen} title="Water 7" />
+
+        <Space height={8} />
+      </ScrollView>
     </View>
   );
 };
