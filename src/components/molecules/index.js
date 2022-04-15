1b30da890f1fae90b91226fb83b40631202e2d05
unknown <izzy.zammil@gmail.com>
2022-04-15 22:54:00 +0700

    Add ListProfile Component in ProfileScreen

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -3,5 +3,13 @@ import BottomNavigator from './BottomNavigator';
 import VacationCard from './VacationCard';
 import CategoriesCard from './CategoriesCard';
 import EmptyFavourite from './EmptyFavourite';
+import ListProfile from './ListProfile';
 
-export {Header, BottomNavigator, VacationCard, CategoriesCard, EmptyFavourite};
+export {
+  Header,
+  BottomNavigator,
+  VacationCard,
+  CategoriesCard,
+  EmptyFavourite,
+  ListProfile,
+};
--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -1,10 +1,53 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
 import React from 'react';
+import {Button, Header, ListProfile, Space} from '../../components';
+import {DummyProfile} from '../../assets';
+import {COLORS} from '../../constants';
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+      <Header title="Profile" type="edit-profile" />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{paddingHorizontal: 24}}>
+        <View style={{alignItems: 'center', marginBottom: 26, marginTop: 24}}>
+          <Image
+            source={DummyProfile}
+            style={{
+              width: 125,
+              height: 125,
+              borderRadius: 125,
+              marginBottom: 12,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: 'Poppins-Bold',
+              color: COLORS.primary2,
+              fontSize: 16,
+            }}>
+            John Doe
+          </Text>
+          <Text style={{fontFamily: 'Poppins-Regular', color: COLORS.primary2}}>
+            San Francisco, CA
+          </Text>
+        </View>
+
+        <View>
+          <ListProfile titleName="Username" valueName="JohnDoe" />
+          <ListProfile titleName="Email" valueName="johndoe@gmail.com" />
+          <ListProfile titleName="Phone" valueName="081234567891" />
+          <ListProfile
+            titleName="Address"
+            valueName="6391 Elgin St Celina, Delaware"
+          />
+        </View>
+        <Space height={24} />
+        <Button labelBtn="Sign Out" />
+        <Space height={24} />
+      </ScrollView>
     </View>
   );
 };
