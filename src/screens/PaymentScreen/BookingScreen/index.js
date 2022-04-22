d85edc1c832f994f648ccf6bcb279677fdc83a6e
unknown <izzy.zammil@gmail.com>
2022-04-22 16:31:02 +0700

    Refactory BookingScreen

--- src/screens/PaymentScreen/BookingScreen/index.js
+++ src/screens/PaymentScreen/BookingScreen/index.js
@@ -10,54 +10,25 @@ const BookingScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+    <View style={styles.page}>
       <Header title="Booking Information" type="onback-payment" noStepper="1" />
       <Space height={24} />
-      <View style={{paddingHorizontal: 24, flex: 1}}>
-        <Text
-          style={{
-            fontFamily: 'Poppins-Regular',
-            color: COLORS.primary2,
-            marginBottom: 8,
-          }}>
-          Item Booking
-        </Text>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-          <Image
-            source={DummyDetailScreen}
-            style={{height: 66, width: 66, borderRadius: 8}}
-          />
-          <View style={{marginLeft: 12, flex: 1}}>
-            <Text
-              style={{
-                fontFamily: 'Poppins-Regular',
-                color: COLORS.primary2,
-                fontSize: 16,
-              }}>
-              Ocean Land
-            </Text>
-            <Text style={{fontFamily: 'Poppins-Regular', color: COLORS.gray}}>
-              14.200.000 IDR
-            </Text>
-            <Text style={{fontFamily: 'Poppins-Regular', color: COLORS.gray}}>
-              20 Jan - 22 Jan
-            </Text>
+
+      <View style={styles.container}>
+        <Text style={styles.textLabel}>Item Booking</Text>
+        <View style={styles.itemBookingContent}>
+          <Image source={DummyDetailScreen} style={styles.image} />
+          <View style={subItemBookingContent}>
+            <Text style={styles.textTitle}>Ocean Land</Text>
+            <Text style={styles.text}>14.200.000 IDR</Text>
+            <Text style={styles.text}>20 Jan - 22 Jan</Text>
           </View>
-          <Text style={{fontFamily: 'Poppins-Regular', color: COLORS.gray}}>
-            2 nights
-          </Text>
+          <Text style={styles.text}>2 nights</Text>
         </View>
 
         <Divider marginBottom={20} marginTop={20} />
 
-        <Text
-          style={{
-            fontFamily: 'Poppins-Regular',
-            color: COLORS.primary2,
-            marginBottom: 8,
-          }}>
-          Details Transaction
-        </Text>
+        <Text style={styles.textLabel}>Details Transaction</Text>
         <ItemValue label={'Sub Total :'} value={'14.200.000 IDR'} />
         <ItemValue label={'Tax 10% :'} value={'1.420.000 IDR'} />
         <ItemValue
@@ -67,19 +38,13 @@ const BookingScreen = () => {
         />
 
         <Divider marginBottom={20} marginTop={20} />
-        <Text
-          style={{
-            fontFamily: 'Poppins-Regular',
-            color: COLORS.primary2,
-            marginBottom: 8,
-          }}>
-          Info
-        </Text>
+
+        <Text style={styles.textLabel}>Info</Text>
         <ItemValue label="Name :" value={'John Doe'} />
         <ItemValue label="Phone No :" value={'081234567890'} />
       </View>
 
-      <View style={{paddingHorizontal: 24, marginBottom: 24}}>
+      <View style={styles.buttonContainer}>
         <Button
           labelBtn="Continue to Book"
           onPress={() => navigation.navigate('TransferBankScreen')}
@@ -91,4 +56,22 @@ const BookingScreen = () => {
 
 export default BookingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {flex: 1, backgroundColor: COLORS.white},
+  container: {paddingHorizontal: 24, flex: 1},
+  textLabel: {
+    fontFamily: 'Poppins-Regular',
+    color: COLORS.primary2,
+    marginBottom: 8,
+  },
+  itemBookingContent: {flexDirection: 'row', alignItems: 'center'},
+  image: {height: 66, width: 66, borderRadius: 8},
+  subItemBookingContent: {marginLeft: 12, flex: 1},
+  textTitle: {
+    fontFamily: 'Poppins-Regular',
+    color: COLORS.primary2,
+    fontSize: 16,
+  },
+  text: {fontFamily: 'Poppins-Regular', color: COLORS.gray},
+  buttonContainer: {paddingHorizontal: 24, marginBottom: 24},
+});
