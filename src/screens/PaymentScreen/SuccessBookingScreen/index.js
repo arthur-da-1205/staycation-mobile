d2003d911640cfe62c1342a046ebf17383f97d00
unknown <izzy.zammil@gmail.com>
2022-04-22 16:41:44 +0700

    Refactory SuccessBookingScreen

--- src/screens/PaymentScreen/SuccessBookingScreen/index.js
+++ src/screens/PaymentScreen/SuccessBookingScreen/index.js
@@ -8,25 +8,19 @@ import {useNavigation} from '@react-navigation/native';
 const SuccessBookingScreen = () => {
   const navigation = useNavigation();
   return (
-    <View style={{flex: 1, backgroundColor: COLORS.white}}>
+    <View style={styles.page}>
       <Header title="Yay! Completed" type="payment" noStepper="3" />
       <Space height={36} />
-      <View style={{paddingHorizontal: 24, alignItems: 'center', flex: 1}}>
+
+      <View style={styles.container}>
         <IlSuccessBooking />
-        <Text
-          style={{
-            fontFamily: 'Poppins-Regular',
-            color: COLORS.gray,
-            marginTop: 24,
-            textAlign: 'center',
-            paddingHorizontal: 22,
-          }}>
+        <Text style={styles.text}>
           We will inform you via email later once the transaction has been
           accepted
         </Text>
       </View>
 
-      <View style={{paddingHorizontal: 24, marginVertical: 24}}>
+      <View style={styles.buttonContainer}>
         <Button
           labelBtn="Back to Home"
           onPress={() => navigation.navigate('MainApp')}
@@ -38,4 +32,15 @@ const SuccessBookingScreen = () => {
 
 export default SuccessBookingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {flex: 1, backgroundColor: COLORS.white},
+  container: {paddingHorizontal: 24, alignItems: 'center', flex: 1},
+  text: {
+    fontFamily: 'Poppins-Regular',
+    color: COLORS.gray,
+    marginTop: 24,
+    textAlign: 'center',
+    paddingHorizontal: 22,
+  },
+  buttonContainer: {paddingHorizontal: 24, marginVertical: 24},
+});
