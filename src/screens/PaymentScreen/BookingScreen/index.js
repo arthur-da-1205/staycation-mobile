2a2cab74ab0e72abfd6426a56c84ffff92f15f1d
unknown <izzy.zammil@gmail.com>
2022-04-22 14:32:16 +0700

    Slicing BookingScreen

--- src/screens/PaymentScreen/BookingScreen/index.js
+++ src/screens/PaymentScreen/BookingScreen/index.js
@@ -1,16 +1,16 @@
 import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
-import {Header, ItemValue, Space} from '../../../components';
+import {Button, Header, ItemValue, Space} from '../../../components';
 import {DummyDetailScreen} from '../../../assets';
 import Divider from '../../../components/molecules/Divider';
 import {COLORS} from '../../../constants';
 
 const BookingScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, backgroundColor: COLORS.white}}>
       <Header title="Booking Information" type="onback-payment" noStepper="1" />
       <Space height={24} />
-      <View style={{paddingHorizontal: 24}}>
+      <View style={{paddingHorizontal: 24, flex: 1}}>
         <Text
           style={{
             fontFamily: 'Poppins-Regular',
@@ -55,7 +55,29 @@ const BookingScreen = () => {
           }}>
           Details Transaction
         </Text>
-        <ItemValue label={'Sub Total:'} value={'14.200.000 IDR'} />
+        <ItemValue label={'Sub Total :'} value={'14.200.000 IDR'} />
+        <ItemValue label={'Tax 10% :'} value={'1.420.000 IDR'} />
+        <ItemValue
+          label={'Total Price:'}
+          value={'15.220.000 IDR'}
+          valueColor={COLORS.green}
+        />
+
+        <Divider marginBottom={20} marginTop={20} />
+        <Text
+          style={{
+            fontFamily: 'Poppins-Regular',
+            color: COLORS.primary2,
+            marginBottom: 8,
+          }}>
+          Info
+        </Text>
+        <ItemValue label="Name :" value={'John Doe'} />
+        <ItemValue label="Phone No :" value={'081234567890'} />
+      </View>
+
+      <View style={{paddingHorizontal: 24, marginBottom: 24}}>
+        <Button labelBtn="Continue to Book" />
       </View>
     </View>
   );
